db7694527c189bb62a682d796cf4f9a6d5f21716
Eduardo <yo+gitea@adderou.cl>
2019-04-23 01:28:27 -0400

    Separating DTC in other files. Starting with config.

---
+++ config.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/dtc/")
+	viper.AddConfigPath("$HOME/.dtc")
+	viper.AddConfigPath("./")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("config file not found! %v", err))
+	}
+}
+
+type Config struct {
+	DTC      DTCConfig
+	Criptoki CriptokiConfig
+}
+
+type DTCConfig struct {
+	PublicKey  string
+	PrivateKey string
+	InstanceId string
+	Nodes      []*NodesConfig
+}
+
+type NodesConfig struct {
+	IP         string
+	PublicKey  string
+	SubPort    uint16
+	DealerPort uint16
+}
+
+type CriptokiConfig struct {
+	DatabasePath string
+	NodesNumber  uint16
+	Threshold    uint16
+	Slots        []*SlotsConfig
+	Timeout      uint16
+}
+
+type SlotsConfig struct {
+	Label string
+}
+
+func GetConfig() (*Config, error) {
+	var conf Config
+	err := viper.Unmarshal(&conf)
+	if err != nil {
+		return nil, err
+	}
+	return &conf, nil
+}
