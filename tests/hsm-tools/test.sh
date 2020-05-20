0ce6eff2867327c90b712ba539cabf9d335c5b13
Eduardo <eriveros@dcc.uchile.cl>
2020-05-20 19:28:05 -0400

    adapting test scripts to new dtcconfig command name

--- tests/hsm-tools/test.sh
+++ tests/hsm-tools/test.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname $0)" || exit
 docker-compose build
-go run github.com/niclabs/dtcconfig rsa \
+go run github.com/niclabs/dtcconfig create \
   -n dtcnode1:2030,dtcnode2:2030,dtcnode3:2030,dtcnode4:2030,dtcnode5:2030 \
   -t 3 \
   -H "dtcclient" \
